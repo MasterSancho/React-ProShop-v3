715b6189c9dc9a3f44d0c5117646e793cc3ca6c1
mastersancho <1maltsevalex@gmail.com>
2020-12-10 16:29:48 +0200

    017-Getting Started With MongoDB
    => Custom Error Handing

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -23,10 +23,10 @@ router.get(
     asyncHandler(async (req, res) => {
         const product = await Product.findById(req.params.id);
 
-        if (condition) {
+        if (product) {
             res.json(product);
         } else {
-            res.status(404).json({ message: 'Product not found' });
+            throw new Error('Product not found');
         }
     })
 );
